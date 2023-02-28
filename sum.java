ac455cc02ac2476e0405c910a2acf30a286d6628
devopsnhc07 <devopsnhc07@gmail.com>
2023-02-28 12:56:50 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum
+{
+public static void main(string[]args)
+{
+int a=,b,sum;
+ System.out.println("enter the values");
+sum=a+b;
+Sysytem.out.println("the sum of numbers is"+sum);
+}
+}
