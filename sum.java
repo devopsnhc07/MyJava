598e70160a91e9df12688290f3ac40d07de83f06
devopsnhc07 <devopsnhc07@gmail.com>
2023-02-28 13:08:27 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -3,8 +3,12 @@ public class sum
 {
 public static void main(string[]args)
 {
-int a=,b,sum;
- System.out.println("enter the values");
+int a,b,sum;
+ scanner sc=new scanner(system.in);
+ system.out.println("enter the first value");
+ a=sc.nextint();
+ system.out.println("enter the sec value");
+ b=sc.nextint();
 sum=a+b;
 Sysytem.out.println("the sum of numbers is"+sum);
 }
