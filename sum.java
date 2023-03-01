a3f0faacd9802fee4ecf4b82a24be9b0ade8daa7
devopsnhc07 <devopsnhc07@gmail.com>
2023-03-01 09:50:42 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -4,9 +4,9 @@ public class sum
 public static void main(String[]args)
 {
 System.out.println("enter the first value");
-int a= Integer.parseint(args[0]);
+int a= Integer.parseInt(args[0]);
  System.out.println("enter the sec value");
-int b= Integer.parseint(args[1]);
+int b= Integer.parseInt(args[1]);
 int sum;
 sum=a+b;
 System.out.println("the sum of numbers is"+sum);
